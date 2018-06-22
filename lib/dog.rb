7484ee6176d7a7b3e9ae9200915d741723bc4a26
Alyssampratt <magoopratt@gmail.com>
2018-06-22 20:40:14 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,20 @@
 # dog.rb
+class Dogs(good,cute,trained)
+  def initialize
+@good = good
+@cute = cute
+@trained = trained
+end 
+
+def is_it_good
+  @good
+end 
+
+def is_it_cute
+  @cute
+end
+
+def is_it_trained
+  @trained
+end
+
